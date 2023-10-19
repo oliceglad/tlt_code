962b2339a31f2b9cd62215b4b56709b6f48304c8
oliceglad <vycheslavofficial@gmail.com>
2023-10-19 11:49:45 +0400

    initial commit

---
+++ code-back/routes.py
@@ -0,0 +1,10 @@
+from flask import Flask, request
+from flask_restful import Api, Resource
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+api = Api(app)
+class GetFile(Resource):
+    def post(self):
+        return {'data': request.args}
