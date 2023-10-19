89083f55832260a853601811b4da9ac3e6572f2f
oliceglad <vycheslavofficial@gmail.com>
2023-10-19 13:28:30 +0400

    added uploadFile

--- code-front/src/Components/UploadFile.jsx
+++ code-front/src/Components/UploadFile.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import s from './upload.module.scss'
+import { connect } from 'react-redux';
+import { postDataFile } from "../redux/main-reducer";
 
 
-export const UploadFile = () => {
+const UploadFile = ({postDataFile, data}) => {
     const [drag, setDrag] = useState(false)
 
     const dragStartHandler = (e) => {
@@ -21,6 +23,9 @@ export const UploadFile = () => {
         let files = [...e.dataTransfer.files]
         const formData = new FormData()
         formData.append('file', files[0])
+
+        
+        postDataFile(formData)
         setDrag(false)
     }
 
@@ -33,7 +38,7 @@ export const UploadFile = () => {
                     className={s.dragArea}
                     onDragStart={e => dragStartHandler(e)}
                     onDragLeave={e => dragLeaveHandler(e)}
-                    onDragOver = {e => dragStartHandler(e)}
+                    onDragOver={e => dragStartHandler(e)}
                     onDrop={e => onDropHandler(e)}
                 >Отпустите файлы, чтобы загрузить их</div>
                 : <div
@@ -45,3 +50,10 @@ export const UploadFile = () => {
         </div>
     )
 }
+
+
+const mapStateToProps = (state) => ({
+    data: state.main.data
+})
+
+export const UploadFileContainer = connect(mapStateToProps, { postDataFile })(UploadFile)
