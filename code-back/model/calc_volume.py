65d4d73afdd523a11d522c574e8293bd711c57dd
oliceglad <vycheslavofficial@gmail.com>
2023-10-19 22:13:59 +0400

    changed calcVolume

--- code-back/model/calc_volume.py
+++ code-back/model/calc_volume.py
@@ -1,10 +1,12 @@
 import math
 
 def calc_formula(length_one, length_two, width):
-    height = (length_two - length_one) / 2
-    return 1/3 * math.pi * ( width /2 ) * height + 1/2 * width * (length_two - length_one) * height
+    height = width * 0.5
+    return (1/3 * math.pi * ( width / 2 ) ** 2 * height + 1/2 * width * (length_two - length_one) * height) / 1000000
 
 class CalcVolume:
     @staticmethod
     def calc_volume_stone(form):
-        return calc_formula(1,2,3)
+        return calc_formula(300,300,300)
+
+print(CalcVolume.calc_volume_stone({}))
